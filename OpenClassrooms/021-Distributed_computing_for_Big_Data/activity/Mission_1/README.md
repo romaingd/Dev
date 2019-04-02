fc11281e8be025533b7ce3e0f259e14a3e030183
romaingd <34568327+romaingd@users.noreply.github.com>
2019-04-02 16:11:19 +0200

    Renaming

--- OpenClassrooms/021-Distributed_computing_for_Big_Data/activity/Mission_1/README.md
+++
@@ -1,3 +0,0 @@
-~~~python
-# for line in ['lake\tdefoe\t0.6', 'lake\tother\t0.4', 'pole\tdefoe\t0.0', 'pole\tother\t0.4']:
-~~~
--- OpenClassrooms/021-Distributed_computing_for_Big_Data/activity/Mission_1/setup_input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hadoop=$HADOOP_HOME/bin/hadoop
-$hadoop fs -mkdir -p input
-wget http://www.textfiles.com/etext/FICTION/defoe-robinson-103.txt
-$hadoop fs -copyFromLocal defoe-robinson-103.txt input
-wget http://www.textfiles.com/etext/FICTION/callwild
-$hadoop fs -copyFromLocal callwild input
-$hadoop fs -ls input
