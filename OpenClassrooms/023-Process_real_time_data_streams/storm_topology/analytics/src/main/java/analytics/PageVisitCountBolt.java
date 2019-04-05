d573230a507bb9c5c2da41044b8512323fa2f2e9
romaingd <34568327+romaingd@users.noreply.github.com>
2019-04-05 16:27:14 +0200

    Storm topology example

---
+++ OpenClassrooms/023-Process_real_time_data_streams/storm_topology/analytics/src/main/java/analytics/PageVisitCountBolt.java
@@ -0,0 +1,33 @@
+package analytics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class PageVisitCountBolt extends BaseRichBolt {
+	private OutputCollector outputCollector;
+	private HashMap<String, Integer> pageVisitCounts;
+
+	@Override
+	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+		pageVisitCounts = new HashMap<String, Integer>();
+		outputCollector = collector;
+	}
+
+	@Override
+	public void execute(Tuple input) {
+		String url = input.getStringByField("url");
+		pageVisitCounts.put(url, pageVisitCounts.getOrDefault(url, 0) + 1);
+		// Notify the spout that the tuple has been correctly processed
+		outputCollector.ack(input);
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+	}
+}
