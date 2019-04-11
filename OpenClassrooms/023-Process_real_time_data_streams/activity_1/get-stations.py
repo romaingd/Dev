205b3ccbea9beb245491a158ef067048e304fc71
romaingd <34568327+romaingd@users.noreply.github.com>
2019-04-11 17:54:28 +0200

    Fixed Kafka activity

--- OpenClassrooms/023-Process_real_time_data_streams/activity_1/get-stations.py
+++ OpenClassrooms/023-Process_real_time_data_streams/activity_1/get-stations.py
@@ -5,10 +5,10 @@ import urllib.request
 from kafka import KafkaProducer
 
 
-API_KEY = "XXX"
+API_KEY = "aaa0efe61838d87ed32868f37ef8e967a31e43ff"
 url = "https://api.jcdecaux.com/vls/v1/stations?apiKey={}".format(API_KEY)
 
-producer = KafkaProducer(bootstrap_server=["localhost:9092", "localhost:9093"])
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
 # Store the status of stations, grouped by city
 stations_empty = {}
@@ -21,28 +21,28 @@ while True:
         # Get info about the station
         station_number = station["number"]
         contract = station["contract_name"]
-        available_bike_stands = station["available_bike_stands"]
+        available_bikes = station["available_bikes"]
 
         # Find past information about the station
-        if contract not in stations:
+        if contract not in stations_empty:
             stations_empty[contract] = {}
         city_stations_empty = stations_empty[contract]
 
         if station_number not in city_stations_empty:
-            city_stations_empty[station_number] = (available_bike_stands == 0)
+            city_stations_empty[station_number] = (available_bikes == 0)
 
         station_was_empty = city_stations_empty[station_number]
-        station_is_empty = (available_bike_stands > 0)
+        station_is_empty = (available_bikes == 0)
 
         # Send a message if the station changed "emptiness status"
         if (station_is_empty ^ station_was_empty):
             producer.send(
                 "empty-stations",
                 json.dumps(station).encode(),
-                key=str(station["number"].encode())  # same stat. -> same part.
+                key=str(station["number"]).encode()  # same stat. -> same part.
             )
         
-        city_stations_empty[station_number] = (available_bike_stands == 0)
+        stations_empty[contract][station_number] = station_is_empty
 
     print("{} Produced {} station records".format(time.time(), len(stations)))
     time.sleep(1)
--- OpenClassrooms/023-Process_real_time_data_streams/activity_1/monitor-empty-stations.py
+++ OpenClassrooms/023-Process_real_time_data_streams/activity_1/monitor-empty-stations.py
@@ -4,7 +4,7 @@ from kafka import KafkaConsumer
 stations_empty = {}
 consumer = KafkaConsumer(
     "empty-stations",
-    bootstrap_server=["localhost:9092", "localhost:9093"],
+    bootstrap_servers="localhost:9092",
     group_id="monitor-empty-stations"
 )
 
@@ -15,7 +15,7 @@ for message in consumer:
     station_number = station["number"]
     address = station["address"]
     contract = station["contract_name"]
-    available_bike_stands = station["available_bike_stands"]
+    available_bikes = station["available_bikes"]
 
     # Find past information about the station
     if contract not in stations_empty:
@@ -23,13 +23,13 @@ for message in consumer:
     city_stations_empty = stations_empty[contract]
 
     if station_number not in city_stations_empty:
-        city_stations_empty[station_number] = (available_bike_stands == 0)
+        city_stations_empty[station_number] = (available_bikes == 0)
     
     # Since a message was sent, the station is either:
     #   * empty now, but was not before
     #   * not empty now, but was before
     # A single boolean is enough to check which case it is.
-    station_is_empty = (available_bike_stands == 0)
+    station_is_empty = (available_bikes == 0)
     city_stations_empty[station_number] = station_is_empty
 
     if station_is_empty:
